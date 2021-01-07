87b72744becfcc9cf8335bdd6be5dd75bf697f03
D-bugging <archmelo@protonmail.com>
2021-01-07 14:57:27 -0300

    Initial commit

---
+++ ExemploMVC-SalesWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ExemploMVC_SalesWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
